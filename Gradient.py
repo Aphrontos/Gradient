fd354a77516bd0f35632f2f1a67bb28b2e334793
Adiáphoros <47263323+Aphrontos@users.noreply.github.com>
2022-04-05 15:19:35 +0800

    Add files via upload

---
+++ Gradient.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 13:30:10 2022
+
+Generates a representation of the sieve of Euler.
+
+@author: Adíaphoros
+"""
+
+import png #pip install pypng
+
+# Gashler M. (2019, April 16) Creating a PNG file in Python
+#	Retrieved March 20 2022, from https://stackoverflow.com/a/55715162/15043016
+# Generate the png
+
+def generate_frames():
+    
+    '''
+    R = Red value
+    G = Green value
+    G = Blue value
+    '''
+    
+    for G in range(256):
+        img = []
+        for R in range(256):
+            row = ()
+            for B in range(256):
+                color = (R, G, B)
+                row += color
+            
+            img.append(row)
+        
+        with open(f'results/{G}.png', 'wb') as f:
+            w = png.Writer(256, 256, greyscale=False)
+            w.write(f, img)
+        print(f'Generated {G:X}.png')
+        
+def main():
+    generate_frames()
+    print("Generated the frames")
+    
+if __name__ == "__main__":
+    main()

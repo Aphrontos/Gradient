58d38e963638c7f4f5706b3696f4b9c86516f18a
Adiáphoros <47263323+Aphrontos@users.noreply.github.com>
2022-04-05 20:16:14 +0800

    Update Gradient.py

--- Gradient.py
+++ Gradient.py
@@ -14,7 +14,7 @@ import png #pip install pypng
 import os
 
 # Gashler M. (2019, April 16) Creating a PNG file in Python
-#	Retrieved March 20 2022, from https://stackoverflow.com/a/55715162/15043016
+#   Retrieved March 20 2022, from https://stackoverflow.com/a/55715162/15043016
 # Generate the png
 
 def generate_frames():
